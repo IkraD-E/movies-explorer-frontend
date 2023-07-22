f85b44721a3f90c73596812688e95f55b3e4d172
Ikra-D <kino0.512321@gmail.com>
2023-07-22 20:34:07 +0800

    Make project carcase

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const thisYear = new Date().getFullYear();
-
-export default function Footer() {
-    return (
-        <footer className="footer">
-          <p className="footer__copyright">© {thisYear} Mesto Russia IkraD</p>
-        </footer>
-    )
-}
