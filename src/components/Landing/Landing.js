28a7745511cabbd77481312921d75767e0d9fad9
Pavel Evtushenko <pasha.512321@gmail.com>
2023-07-28 16:19:59 +0800

    add about

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,7 +6,7 @@ import "./Landing.css"
 function Landing() {
     return (
       <main className="landing">
-        <section className="landing">
+        <section className="section section_grey">
           <div className="landing__main">
             <div className="landing__info">
               <h1 className="landing__header">
@@ -21,6 +21,55 @@ function Landing() {
             </div>
             <img className="landing__picture" src={landingImage} alt="Логотип"/>
           </div>
+        </section>
+        <section className="section" id="about">
+          <div className="about__container_header">
+            <h2 className="about__header">
+              О проекте
+            </h2>
+          </div>
+          <ul className="about__list">
+            <li className="about__element">
+              <h3 className="container__header">
+                Дипломный проект включал 5 этапов
+              </h3>
+              <p className="container__text">
+                Составление плана, работу над бэкендом, 
+                вёрстку, добавление функциональности и финальные доработки.
+              </p>
+            </li>
+            <li className="about__element">
+              <h3 className="container__header">
+                На выполнение диплома ушло 5 недель
+              </h3>
+              <p className="container__text">
+                У каждого этапа был мягкий и жёсткий дедлайн, 
+                которые нужно было соблюдать, чтобы успешно защититься.
+              </p>
+            </li>
+          </ul>
+          <ul className="elapsed-time__list">
+            <li className="elapsed-time__element">
+              <p className="elapsed-itme__text">
+                1 неделя
+              </p>
+            </li>
+            <li className="elapsed-time__element elapsed-time__element_long">
+              <p className="elapsed-itme__text">
+                4 недели
+              </p>
+            </li>
+            <li className="elapsed-time__element">
+              <p className="elapsed-itme__text">
+                Back-end
+              </p>
+            </li>
+            <li className="elapsed-time__element elapsed-time__element_long">
+              <p className="elapsed-itme__text">
+                Front-end
+              </p>
+            </li>
+          </ul>
         </section>
       </main>
     );
