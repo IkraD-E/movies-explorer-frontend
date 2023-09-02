b08a2f5c4cec966e352cdc10a7d4850f89bbad4b
Pavel Evtushenko <pasha.512321@gmail.com>
2023-09-02 14:18:59 +0800

    Add outline

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -27,37 +27,41 @@ function Profile({onSubmit, children}) {
           onSubmit={handleSubmit}
         >
           <div className="profile__input-container">
-            <label htmlFor="name" className="profile__label">Имя</label>
-                <input 
-                required 
-                minLength="2" 
-                maxLength="30" 
-                type="text" 
-                id="name"
-                name="name"
-                className={`profile__input ${errors.name ? ("profile__input_invalid") : ""}`}
-                placeholder="Введите имя" 
-                value={values.name || ""}
-                onChange={handleChange}
-                />
-                <span className={`profile__error name-error ${errors.name && "profile__error_active"}`}>{errors.name || ""}</span>
-          </div>
-          <div className="profile__input-container">
-            <label htmlFor="email" className="profile__label">E-mail</label>
-            <input 
+            <div className="profile__input-box">
+              <label htmlFor="name" className="profile__label">Имя</label>
+              <input 
               required 
               minLength="2" 
               maxLength="30" 
-              type="email" 
-              id="email"
-              name="email"
-              className={`profile__input ${errors.email ? ("profile__input_invalid") : ""}`}
-              placeholder="Введите Email"
-              value={values.email || ""}
+              type="text" 
+              id="name"
+              name="name"
+              className={`profile__input ${errors.name ? ("profile__input_invalid") : ""}`}
+              placeholder="Введите имя" 
+              value={values.name || ""}
               onChange={handleChange}
-            />
-            <span className={`profile__error email-error ${errors.email && "profile__error_active"}`}>{errors.email || ""}</span>
+              />
+            </div>
+            <span className={`profile__error name-error ${errors.name && "profile__error_active"}`}>{errors.name || ""}</span>
           </div>
+          <div className="profile__input-container">
+            <div className="profile__input-box">
+              <label htmlFor="email" className="profile__label">E-mail</label>
+              <input 
+                required 
+                minLength="2" 
+                maxLength="30" 
+                type="email" 
+                id="email"
+                name="email"
+                className={`profile__input ${errors.email ? ("profile__input_invalid") : ""}`}
+                placeholder="Введите Email"
+                value={values.email || ""}
+                onChange={handleChange}
+              />
+            </div>
+            <span className={`profile__error email-error ${errors.email && "profile__error_active"}`}>{errors.email || ""}</span>
+            </div>
           <button 
             className={`profile__btn ${isValid ? "" : "profile__submit_disabled"}`}
             type="submit"
